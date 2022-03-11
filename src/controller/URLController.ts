2db51d9ba594fc8902d3f922609417758081bf21
ShinkuDani <daniel-silvasales@hotmail.com>
2022-03-11 14:38:18 -0300

    arrumando as configurções V1

---
+++ src/controller/URLController.ts
@@ -0,0 +1,39 @@
+import {Request, Response} from 'express'
+import shortid from 'shortid'
+import { config } from 'config/Constants'
+import { URLModel } from 'database/model/URL'
+
+export class URLController {
+    public async shorten(req: Request, response: Response):Promise<void>{
+        const{originURL} = req.body
+        const url = await URLModel.findOne({originURL})
+        if(url){
+            response.json(url)
+            return
+        }
+
+        const hash = shortid.generate()
+        const shortURL = `${config.API_URL} / ${hash}`
+        const newURL = await URLModel.create({originURL, hash, shortURL, })
+        response.json(newURL)
+    }
+
+    public async redirect(req: Request, response: Response): Promise<void>{
+        const { hash } = req.params
+        const url = await URLModel.findOne({hash})
+
+        if(url){
+            response.redirect(url.originURL)
+            return
+        }
+
+        const url ={
+            originURL:"",
+            hash:"",
+            short: ""
+        }
+
+        response.status(400).json({error: 'URL not Found'})
+    }
+    
+}
